adbb5f27857e8ea052e0ff76236d97e8af967db2
Klinikova <klinikova@gmail.com>
2018-05-24 20:55:49 -0700

    init commit

---
+++ src/customexceptionclasses/NegativeStartingBalance.java
@@ -0,0 +1,14 @@
+
+package customexceptionclasses;
+
+public class NegativeStartingBalance extends Exception{
+
+    /**
+     * @param args the command line arguments
+     */
+        public NegativeStartingBalance(double amount){
+            super("That's an error. " +
+                    "Negative starting balance: " + amount);
+        }
+  
+}
